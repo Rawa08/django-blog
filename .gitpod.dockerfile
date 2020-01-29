e3b4a7b2e65cd93e3f35c29360cd8e2de892a22d
Rawa08 <rawa_aref@hotmail.com>
2020-01-29 16:07:42 +0000

    installing heroku

---
+++ .gitpod.dockerfile
@@ -0,0 +1,2 @@
+# Setup Heroku CLI
+RUN curl https://cli-assets.heroku.com/install.sh | sh
---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+
+os.environ.setdefault('SECRET_KEY_BLOG', "'sae)56c@qql)1vvkt8g(@mulm7^qs+%x24)d0f^xy0-7fd*-i8'")

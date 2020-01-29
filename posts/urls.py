a54e9e447b97c13f0d0d8b4503bc150b99a9c27c
Rawa08 <rawa_aref@hotmail.com>
2020-01-29 10:08:21 +0000

    add css

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from .views import get_posts, post_detail, create_or_edit_a_post
 
 urlpatterns = [
    
-    url(r'^$', get_posts, name='get_posts')),
+    url(r'^$', get_posts, name='get_posts'),
     url(r'^(?P<pk>\d+)/$', post_detail, name='post_detail'),
     url(r'^new/$', create_or_edit_a_post, name='new_post'),
     url(r'^(?P<pk>\d+)/edit/$', create_or_edit_a_post, name='edit_post')
--- posts/views.py
+++ posts/views.py
@@ -22,7 +22,7 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     post.views += 1
     post.save()
-    return render(request, 'post_detail.html', {'post'=post})
+    return render(request, 'post_detail.html', {'post': post})
 
 
 
